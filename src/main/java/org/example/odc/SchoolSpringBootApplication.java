289073d5820e8e855ec83ad4761c4b532a3e19b2
b13-niass <barhamadieng66@gmail.com>
2024-09-20 22:16:27 +0000

    first commit

---
+++ src/main/java/org/example/odc/SchoolSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.odc.schoolspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolSpringBootApplication.class, args);
+    }
+
+}
